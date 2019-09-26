88e763a7994ed9f266a9539f56a922aee6d0a59d
Hitesh.Santwani <Guest1.ICT@mcquilling.com>
2019-09-26 13:15:35 -0400

    updated the plugin

--- AttachmentViewer.js
+++ AttachmentViewer.js
@@ -2,5 +2,5 @@ var windowObjectReference;
 var strWindowFeatures = "height=600,width=1050";
 
 function openRequestedPopup() {
-    window.open('https://www.google.com/', 'Opened window from Plugin', strWindowFeatures);
+    window.open('http://www.google.com/', 'Opened window from Plugin', strWindowFeatures);
 }
